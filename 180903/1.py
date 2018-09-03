d8cc5d68526bd224b71c6ef1072769d2b62b6204
kittyomu <kakugankanka@gmail.com>
2018-09-04 00:42:05 +0900

    test flake8

--- 180903/1.py
+++ 180903/1.py
@@ -1,25 +1,21 @@
 import pygame
-from pygame.locals import *
+
 import sys
 import os
 
 
-
-
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300,200))
-    
+    screen = pygame.display.set_mode((300, 200))
+
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
     pygame.display.set_caption("GAME")
-     
 
     while (1):
-        screen.file((0,0,0))
-        pygame.draw.ellipse(screen,(0.100,0),(50,50,200,100),5)
+        screen.file((0, 0, 0))
+        pygame.draw.ellipse(screen, (0, 100, 0), (50, 50, 200, 100), 5)
         pygame.display.upate()
-
 
         for evet in pygame.event.get():
             if event.type == QUIT:
@@ -28,4 +24,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
