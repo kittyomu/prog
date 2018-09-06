46a66f803a5e510305937a38f76e53bdef27f4a1
kittyomu <kakugankanka@gmail.com>
2018-09-07 00:55:19 +0900

    ruby

--- 180903/2.py
+++ 180903/2.py
@@ -8,14 +8,14 @@ import time
 # 一時的に保存するページのリスト
 linkData = []
 
-keyword = "インターネット" 
+keyword = ""
 max = 200
 
 # 検索結果から各ページのリンク先をmaxページ分だけ取得
 for num in range(0,max,20):
     res = requests.get("https://www.irasutoya.com/search?q="+keyword+"*"+"&max-results=20&start="+str(num)+"&by-date=false*")
     soup = BeautifulSoup(res.text, "lxml")
-    time.sleep(0.1)
+    time.sleep(1)
 
     # Linkの箇所をselect
     links = soup.select("a[href]")
@@ -26,12 +26,14 @@ for num in range(0,max,20):
         if re.search(r"irasutoya.*blog-post.*html$",href):
             if not href in linkData:
                 linkData.append(href)
+                print(linkData,"\r")
 
 # 各ページから画像データのリンクを取得して、画像を保存
 for link in linkData:
     res = requests.get(link)
+    print("res:",res)
     soup = BeautifulSoup(res.text, "lxml")
-    #print(soup)
+    print("soup:",soup)
     # 記事中の画像データを抜き出す
     # class separator -> a の抜き出し
     links = soup.select(".separator > a")
@@ -41,12 +43,14 @@ for link in linkData:
         #print(imageLink)
         # ファイル名の取得
         filename = re.search(r"http.*\/(.*png|.*jpg)$",imageLink)
-        print(filename)
+
+        print("filename:",filename)
+        # デバッグ用にダウンロード先Linkを表示
+        print("imagelink:",imageLink)
         # 画像をダウンロードする
         if filename != None:
             urlreq.urlretrieve(imageLink,"down/"+filename.group(1))
         else:
             print("not match")
-        # デバッグ用にダウンロード先Linkを表示
-        print(imageLink)
+
         time.sleep(1)
