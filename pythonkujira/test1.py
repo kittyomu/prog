87b274c21ceddb287339b1e10b66c66e74871a8f
kittyomu <kakugankanka@gmail.com>
2018-09-29 22:39:11 +0900

    update sc

--- pythonkujira/test1.py
+++ pythonkujira/test1.py
@@ -1,3 +1,9 @@
 from bs4 import BeautifulSoup
 
-fp = open("")
+fp = open("books.html", encoding="utf-8")
+soup = BeautifulSoup(fp, "html.parser")
+
+sel = lambda q : print(soup.select_one(q).string)
+
+sel("#nu")
+sel("")
